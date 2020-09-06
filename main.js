dafb1a84b6985b9c140fc59b91e6ccede77a7866
nadia.co <ngarciaorozco@outlook.com>
2020-09-06 01:08:50 -0600

    portfolio

---
+++ main.js
@@ -0,0 +1,9 @@
+
+/*window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("nav").style.backgroundColor = "red";
+  } else {
+  }
+}*/
